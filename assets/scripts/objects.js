e7cd8879404681b6bc906ff362fa2aa7dddfc895
Rayman Moses <raymanmoses89@gmail.com>
2024-09-21 15:48:04 +0300

    render movie title list

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,13 +1,45 @@
-let person = { 
-    name: 'Max',
-    age:30,
-    hobbies: ['Sports', 'Cooking'],
-    greet: () =>{
-        alert('Hi There');
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const renderMovie = () => {
+    const movieList = document.getElementById('movie-list');
+    if(movies.length === 0){
+        movieList.classList.remove('visible');
+    }else{
+        movieList.classList.add('visible');
     }
+    movieList.innerHTML ='';
+    movies.forEach((movie) => {
+        const movieEl = document.createElement('li');
+        movieEl.textContent = movie.info.title;
+        movieList.append(movieEl);
+
+    });
+}
+
+
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    if(title.trim() === ''||extraName.trim() === ''||extraValue.trim() === '' ){
+        return;
+    }
+
+    const newMovie = {
+        info: {
+            title,
+            [extraName]: extraValue
+        },
+        id: Math.random()
+    };
+
+    movies.push(newMovie);
+    renderMovie();
+    console.log(movies);
 };
 
-person.isAdmin = true; 
-
-console.log(delete person.age);
-console.log( person);
+addMovieBtn.addEventListener('click', addMovieHandler);
