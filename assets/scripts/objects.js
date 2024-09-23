b76102bc350110c1fb086b3bd697673a6ca002b3
Rayman Moses <raymanmoses89@gmail.com>
2024-09-23 10:02:21 +0300

    adding filtter logic

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,8 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovie = () => {
+// This render the movie title to the movie list 
+const renderMovie = (filter = '') => {
     const movieList = document.getElementById('movie-list');
     if(movies.length === 0){
         movieList.classList.remove('visible');
@@ -11,15 +12,29 @@ const renderMovie = () => {
         movieList.classList.add('visible');
     }
     movieList.innerHTML ='';
-    movies.forEach((movie) => {
+
+    
+    const filteredMovie = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
+    console.log(filteredMovie);
+
+    filteredMovie.forEach((movie) => {
         const movieEl = document.createElement('li');
-        movieEl.textContent = movie.info.title;
+        // movieEl.textContent = movie.info.title;
+        let text = movie.info.title + '-';
+        for(const key in movie.info){
+            if(key !== 'title'){
+                text = text + `${key}: ${movie.info[key]}`
+            }
+        } 
+
+        movieEl.textContent = text;
         movieList.append(movieEl);
 
     });
+
 }
 
-
+// This add the movie detail add by the user 
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
     const extraName = document.getElementById('extra-name').value;
@@ -39,7 +54,13 @@ const addMovieHandler = () => {
 
     movies.push(newMovie);
     renderMovie();
-    console.log(movies);
+    // console.log(movies);
 };
 
+const searchMovieHandler = ()=>{
+    const filterTerm = document.getElementById('filter-title').value
+    renderMovie(filterTerm);
+}
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
